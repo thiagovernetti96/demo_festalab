88d6d850dd3a6586f74449e4d9b234fc58a63f2d
root <root@DESKTOP-BUMOQVC.localdomain>
2023-10-09 12:55:49 -0300

    correção para o telefone ter 9 digitos

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "Formato de e-mail inválido" }
   validates :cpf, presence: true, format: { with: /\A\d{3}\.\d{3}\.\d{3}-\d{2}\z/, message: "Formato de CPF inválido" }
-  validates :phone, presence: true, format: { with: /\A\d{10}\z/, message: "Formato de telefone inválido" }
+  validates :phone, presence: true, format: { with: /\A\d{9}\z/, message: "Formato de telefone inválido" }
 end
 
--- spec/models/user.spec.rb
+++ spec/models/user.spec.rb
@@ -3,13 +3,13 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it "valida o formato de email" do
-    user = User.new(name: "Thiago Vernetti", email: "invalid-email", cpf: "123.456.789-01", phone: "1234567890")
+    user = User.new(name: "Thiago Vernetti", email: "invalid-email", cpf: "123.456.789-01", phone: "123456789")
     expect(user).not_to be_valid
     expect(user.errors[:email]).to include("Formato de e-mail inválido")
   end
 
   it "valida o formato do cpf" do
-    user = User.new(name: "Thiago Vernetti", email: "thiago@example.com", cpf: "1243543534534", phone: "1234567890")
+    user = User.new(name: "Thiago Vernetti", email: "thiago@example.com", cpf: "1243543534534", phone: "123456789")
     expect(user).not_to be_valid
     expect(user.errors[:cpf]).to include("Formato de CPF inválido")
   end
